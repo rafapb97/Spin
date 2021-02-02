461b3efd23720d722db9e920c83ceec1b2070640
Matthijs Pals <matthijs-pals@hotmail.com>
2021-02-02 15:48:32 +0100

    removed cache

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import nmpi
-client = nmpi.Client("rafaperez")
+client = nmpi.Client("MatthijsPals")
 job = client.submit_job(source="https://github.com/Matthijspals/Spin",
                         platform=nmpi.SPINNAKER,
                         #config = {"extra_pip_installs": ["snntoolbox"]},
-                        collab_id=89273)
+                        collab_id=89105)
