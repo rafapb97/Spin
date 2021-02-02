209eefdfbd6ba8147bd3f2ab9560368e82e0e770
rafapb97 <rjpbelizon@gmail.com>
2021-02-02 13:50:16 +0100

    again

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import nmpi
-client = nmpi.Client("MatthijsPals")
+client = nmpi.Client("rafaperez")
 job = client.submit_job(source="https://github.com/Matthijspals/Spin",
                         platform=nmpi.SPINNAKER,
                         #config = {"extra_pip_installs": ["snntoolbox"]},
-                        collab_id=89105)
+                        collab_id=89273)
