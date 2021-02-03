9cb0fabcb1fe81cecab7d0b1b62ffcf6c4a3d67c
Matthijs Pals <matthijs-pals@hotmail.com>
2021-02-03 20:59:06 +0100

    updated run once again

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ import pyNN.spiNNaker as pynn
 print (pynn.IF_cond_exp.default_parameters)
 
 #set sim parameters
-sim_time = 500
+sim_time = 50
 dt = 0.1
 
 #load data
@@ -42,7 +42,7 @@ cell_params = {
 layer1 = pynn.Population(13068, pynn.SpikeSourcePoisson(), label='InputLayer')
 #layer1.record("spikes")
 network.append(layer1)
-
+"""
 layer2 = pynn.Population(15376, pynn.IF_curr_exp, cell_params, label='Conv1')
 #layer2.record("spikes")
 network.append(layer2)
@@ -54,7 +54,7 @@ network.append(layer3)
 layer4 = pynn.Population(6728, pynn.IF_curr_exp, cell_params, label='Conv3')
 #layer4.record("spikes")
 network.append(layer4)
-
+"""
 layer5 = pynn.Population(5, pynn.IF_curr_exp, cell_params, label='Output')
 layer5.record("spikes")
 network.append(layer5)
@@ -76,8 +76,7 @@ for i in range(len(network)-1):
 	inh[:,2] /= weight_scale 
 	pynn.Projection(network[i], network[i+1], pynn.FromListConnector(ex, ['weight', 'delay']), receptor_type='excitatory')
 	pynn.Projection(network[i], network[i+1], pynn.FromListConnector(inh, ['weight', 'delay']), receptor_type='inhibitory')
-
-        network[i+1].initialize(v=0.0)
+	network[i+1].initialize(v=0.0)
 #set input
 x_flat = np.ravel(data)
 
@@ -95,38 +94,31 @@ pynn.run(sim_time)
 #get spikes
 spikes_brains = list()
 segments=[]
-for brain in network:
-    spikes_brains.append(brain.get_data("spikes").segments[0].spiketrains)
-    segments.append(brain.get_data("spikes").segments[0])
+spikes_brains.append(network[-1].get_data("spikes").segments[0].spiketrains)
+segments.append(network[-1].get_data("spikes").segments[0])
 
 #end simulation
 pynn.end()
 
 
-fig = plt.figure(figsize=(12, 6))
-plot_spiketrains(segments[4])
-fig.savefig("segment4.png")
-
 #generate some plots
-for i, spikes_brain in enumerate(spikes_brains):
-    fig = plt.figure(figsize=(12, 6))
-    grid = gs.GridSpec(3, 1, height_ratios=(1, 1, 4))
+fig = plt.figure(figsize=(12, 6))
+grid = gs.GridSpec(3, 1, height_ratios=(1, 1, 4))
 
-    ax_spikes = fig.add_subplot(grid[2, 0])
+ax_spikes = fig.add_subplot(grid[2, 0])
 
-    for (nrn, spike_train) in enumerate(spikes_brain):
-        ax_spikes.plot(
-            spike_train, np.ones_like(spike_train) * nrn, "|", c='r', ms=5)
+for (nrn, spike_train) in enumerate(spikes_brains[0]):
+	ax_spikes.plot(spike_train, np.ones_like(spike_train) * nrn, "|", c='r', ms=5)
 
-    mn_spiketimes = [time for spikes in spikes_brain for time in spikes]
-    mn_signal, mn_times = np.histogram(
-        mn_spiketimes, bins=np.arange(0.0, sim_time, 3.0))
+mn_spiketimes = [time for spikes in spikes_brains[0] for time in spikes]
+mn_signal, mn_times = np.histogram(
+mn_spiketimes, bins=np.arange(0.0, sim_time, 3.0))
 
-    ax_spikes.set_xlabel("time [ms] layer" + str(i) )
-    ax_spikes.set_ylabel("")
+ax_spikes.set_xlabel("time [ms] layer" + str(i) )
+ax_spikes.set_ylabel("")
 
-    #ax_spikes.set_yticks(np.arange(0, 10, 1))
-    #ax_spikes.set_yticklabels(["", "", "MN", "", "", "", "", "~MN", "", ""])
-    ax_spikes.set_xlim(-1, sim_time+1)
+#ax_spikes.set_yticks(np.arange(0, 10, 1))
+#ax_spikes.set_yticklabels(["", "", "MN", "", "", "", "", "~MN", "", ""])
+ax_spikes.set_xlim(-1, sim_time+1)
 
-    fig.savefig("spikes layer: " + str(i) + ".png")
+fig.savefig("spikes layer: " + str(i) + ".png")
